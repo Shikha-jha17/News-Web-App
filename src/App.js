46ac9a0cb3d920494cea8ea212dfa09ebc64731f
Shikha-jha17 <jha17.shikha@gmail.com>
2024-03-15 11:44:24 +0530

    News app day 1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import NavbarNewsapp from './components/NavbarNewsapp'
+import News from './components/News'
+import Newsitem from './components/Newsitem'
+
+export default class extends Component {
+ 
+  render() {
+    return (
+      <>
+      <NavbarNewsapp/>
+      <News/>
+      {/*<Newsitem/>*/}
+      
+      
+      
+      
+      
+      
+      
+      </>
+    )
+  }
+}
